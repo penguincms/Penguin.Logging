9644a1e0bd01a02461d942c45d98b2c122430f2a
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 09:37:02 -0700

    Initial Check In

---
+++ Logger.cs
@@ -0,0 +1,169 @@
+﻿using Penguin.Cms.Logging.Entities;
+using Penguin.Errors;
+using Penguin.Messaging.Core;
+using Penguin.Messaging.Logging;
+using Penguin.Messaging.Logging.Extensions;
+using Penguin.Persistence.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Penguin.Logging
+{
+    /// <summary>
+    /// A class provided with the intent of logging out messages through as many code paths as possible to ensure nothing is missed
+    /// </summary>
+    public class Logger : IDisposable
+    {
+        /// <summary>
+        /// Constructs a new instance of this class
+        /// </summary>
+        /// <param name="_caller">The object that is doing the logging</param>
+        /// <param name="messageBus">A message bus to send log messages over</param>
+        /// <param name="logEntryRepository">An IRepository implememntation for persisting logged messages</param>
+        /// <param name="errorRepository">An IRepository implementation for persisting errors</param>
+        public Logger(object _caller, MessageBus messageBus, IRepository<LogEntry> logEntryRepository, IRepository<Error> errorRepository)
+        {
+            this.SessionStart = DateTime.Now;
+            this.GUID = Guid.NewGuid().ToString();
+            this.Caller = _caller.GetType().ToString();
+            this.Entries = new List<LogEntry>();
+            this.MessageBus = messageBus;
+            this.LogEntryRepository = logEntryRepository;
+            this.ErrorRepository = errorRepository;
+            this.FileName = $"Logs\\{Caller}.{SessionStart.ToString("yyyy.MM.dd.HH.mm.ss")}.log";
+
+            if (!Directory.Exists("Logs"))
+            {
+                Directory.CreateDirectory("Logs");
+            }
+
+            WriteContext = logEntryRepository.WriteContext();
+        }
+
+        private string FileName { get; set; }
+        private IWriteContext WriteContext { get; set; }
+        private string Caller { get; set; }
+        private IRepository<LogEntry> LogEntryRepository { get; set; }
+        private IRepository<Error> ErrorRepository { get; set; }
+        private List<LogEntry> Entries { get; set; }
+        private string GUID { get; set; }
+        private DateTime SessionStart { get; set; }
+        private MessageBus MessageBus { get; set; }
+
+        private void LogToFile(string toLog, LogLevel type)
+        {
+            try
+            {
+                File.AppendAllText(this.FileName, $"[{type}] {DateTime.Now.ToString("yyyy.MM.dd.HH.mm.ss")}: {toLog}");
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Logs a text string to the various recievers in this logger
+        /// </summary>
+        /// <param name="toLog">The pre-formatted string to log</param>
+        /// <param name="type">The log level for this message</param>
+        /// <param name="args">Any format arguments for the string</param>
+        public void Log(string toLog, LogLevel type, params object[] args)
+        {
+            LogToFile(string.Format(toLog, args), type);
+
+            LogEntry newEntry = new LogEntry
+            {
+                Caller = this.Caller,
+                Level = type,
+                Session = this.GUID,
+                SessionStart = this.SessionStart
+            };
+
+            MessageBus?.Log(string.Format(toLog, args), type);
+
+            if (args.Length > 0)
+            {
+                newEntry.Value = string.Format(toLog, args);
+            }
+            else
+            {
+                newEntry.Value = toLog;
+            }
+
+            newEntry.DateCreated = DateTime.Now;
+
+#if DEBUG
+            System.Diagnostics.Debug.WriteLine("[" + DateTime.Now.ToString() + "] " + newEntry.Value);
+#else
+
+#endif
+
+            this.Entries.Add(newEntry);
+        }
+
+        /// <summary>
+        /// Logs a debug level message to the various recievers
+        /// </summary>
+        /// <param name="toLog">The preformatted string to log</param>
+        /// <param name="args">Optional arguments to be used during string formatting</param>
+        public void LogDebug(string toLog, params object[] args) => this.Log(toLog, LogLevel.Debug, args);
+
+        /// <summary>
+        /// Logs an error level message to the various recievers
+        /// </summary>
+        /// <param name="toLog">The preformatted string to log</param>
+        /// <param name="args">Optional arguments to be used during string formatting</param>
+        public void LogError(string toLog, params object[] args) => this.Log(toLog, LogLevel.Error, args);
+
+        /// <summary>
+        /// Logs an exception as an exception level message to the various recievers
+        /// </summary>
+        /// <param name="ex">The exception to log</param>
+        public void LogException(Exception ex)
+        {
+            this.LogError(ex.Message);
+            MessageBus?.Log(ex);
+            this.ErrorRepository.AddOrUpdate(new Error(ex));
+        }
+
+        /// <summary>
+        /// Logs an informational message to the various recievers
+        /// </summary>
+        /// <param name="toLog">The preformatted string to log</param>
+        /// <param name="args">Optional arguments to be used during string formatting</param>
+        public void LogInfo(string toLog, params object[] args) => this.Log(toLog, LogLevel.Info, args);
+
+        /// <summary>
+        /// Logs an warning level message to the various recievers
+        /// </summary>
+        /// <param name="toLog">The preformatted string to log</param>
+        /// <param name="args">Optional arguments to be used during string formatting</param>
+        public void LogWarning(string toLog, params object[] args) => this.Log(toLog, LogLevel.Warning, args);
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        /// <summary>
+        /// Disposes of this logger and persists messages to the underlying database for IRepository implementations
+        /// </summary>
+        public void Dispose() => this.Dispose(true);
+
+        /// <summary>
+        /// Disposes of this logger and persists messages to the underlying database for IRepository implementations
+        /// </summary>
+        /// <param name="disposing">unused</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                this.LogEntryRepository.AddOrUpdate(this.Entries.ToArray());
+
+                this.LogEntryRepository.Commit(WriteContext);
+
+                this.disposedValue = true;
+            }
+
+            WriteContext.Dispose();
+        }
+    }
+}
